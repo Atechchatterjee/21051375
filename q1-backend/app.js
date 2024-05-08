c4e91207f7bd4de128cc88fca90d961d3d13bf36
Atechchatterjee <atechchatterjeee@gmail.com>
2024-05-08 13:16:58 +0530

    implemented function to refetch bearer auth token

--- q1-backend/app.js
+++ q1-backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 const app = express();
 const port = 5000;
 const crypto = require("crypto");
+
+app.use(cors());
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
@@ -10,7 +13,7 @@ app.get("/", function (req, res) {
 
 let topProducts = [];
 let bearerToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE1MTUwNDQwLCJpYXQiOjE3MTUxNTAxNDAsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjM3MTVhNTYyLWEzNjktNDk4Ny1hZTVjLTgzM2I0NTRhOTEyYyIsInN1YiI6ImF0ZWNoY2hhdHRlcmplZWVAZ21haWwuY29tIn0sImNvbXBhbnlOYW1lIjoiZ29NYXJ0IiwiY2xpZW50SUQiOiIzNzE1YTU2Mi1hMzY5LTQ5ODctYWU1Yy04MzNiNDU0YTkxMmMiLCJjbGllbnRTZWNyZXQiOiJEZUVRTkx5cE1HVmJ3cWRPIiwib3duZXJOYW1lIjoiQW5pc2ggQ2hhdHRvcGFkaHlheSIsIm93bmVyRW1haWwiOiJhdGVjaGNoYXR0ZXJqZWVlQGdtYWlsLmNvbSIsInJvbGxObyI6IjIxMDUxMzc1In0.tKUGT7qAWEuIYj-xtdaJuA0vy-jURiu6Rr8LdOyBObQ";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE1MTUyNDQ2LCJpYXQiOjE3MTUxNTIxNDYsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjM3MTVhNTYyLWEzNjktNDk4Ny1hZTVjLTgzM2I0NTRhOTEyYyIsInN1YiI6ImF0ZWNoY2hhdHRlcmplZWVAZ21haWwuY29tIn0sImNvbXBhbnlOYW1lIjoiZ29NYXJ0IiwiY2xpZW50SUQiOiIzNzE1YTU2Mi1hMzY5LTQ5ODctYWU1Yy04MzNiNDU0YTkxMmMiLCJjbGllbnRTZWNyZXQiOiJEZUVRTkx5cE1HVmJ3cWRPIiwib3duZXJOYW1lIjoiQW5pc2ggQ2hhdHRvcGFkaHlheSIsIm93bmVyRW1haWwiOiJhdGVjaGNoYXR0ZXJqZWVlQGdtYWlsLmNvbSIsInJvbGxObyI6IjIxMDUxMzc1In0.YJK63iFxMXKV5WkteXsZIpJFXXH2ok30y3TlPSlglxE";
 
 function setUniqueIdentifiersForProducts() {
   topProducts = topProducts.map((prd) => ({
@@ -30,6 +33,23 @@ app.get("/categories/:categoryname/products/:productId", function (req, res) {
   res.json({ product: requiredProduct });
 });
 
+async function refetchBearerToken() {
+  try {
+    const res = await axios.post("http://20.244.56.144/test/auth", {
+      companyName: "goMart",
+      clientId: "3715a562-a369-4987-ae5c-833b454a912c",
+      clientSecret: "DeEQNLypMGVbwqdO",
+      ownerName: "Anish Chattopadhyay",
+      ownerEmail: "atechchatterjeee@gmail.com",
+      rollNo: "21051375",
+    });
+    bearerToken = res.data.access_token;
+    return Promise.resolve(res.data.access_token);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
 app.get(
   "/categories/:categoryname/products/:n?/:page?/:minprice?/:maxprice?",
   async function (req, res) {
@@ -37,6 +57,7 @@ app.get(
     const companyNames = ["AMZ", "FLP", "MYN", "AZO"];
     try {
       let allProducts = [];
+
       for (const companyName of companyNames) {
         const res = await axios.get(
           `http://20.244.56.144/test/companies/${companyName}/categories/${categoryname}/products?top=${n}&minPrice=${minprice}&maxPrice=${maxprice}`,
@@ -59,6 +80,7 @@ app.get(
       });
     } catch (err) {
       console.error(err);
+      // refetchBearerToken();
       return res.sendStatus(400);
     }
   }
